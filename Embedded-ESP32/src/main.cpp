9b81c7286a81151e3fd0108f63e140cae141a945
I-Badier <isaure.badier@gmail.com>
2022-11-24 10:46:07 +0100

    LED button connected and synchronised
    TO DO : interruption on button pin to start (or stop) the blinkers

--- Embedded-ESP32/src/main.cpp
+++ Embedded-ESP32/src/main.cpp
@@ -3,6 +3,8 @@
 
 #define NUM_LED 3
 #define LIGHT_SENSOR_PIN 0
+#define LEFT_BLINKER_BUTTON 5
+#define LEFT_BLINKER_LED 4
 
 #define LEFT_BLINKER 0
 #define RIGHT_BLINKER 1
@@ -16,6 +18,8 @@ void setup()
   Serial.println("-- SETUP --");
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LIGHT_SENSOR_PIN, INPUT);
+  pinMode(LEFT_BLINKER_BUTTON, INPUT);
+  pinMode(LEFT_BLINKER_LED, OUTPUT);
   Serial.println("- END OF SETUP --");
 }
 
@@ -25,13 +29,19 @@ void loop()
   Serial.print("Luminosity : ");
   Serial.println(light_value);
 
+  int button_left_value = digitalRead(LEFT_BLINKER_BUTTON);
+  Serial.print("Left button : ");
+  Serial.println(button_left_value);
+
   long int now = millis();
   leds.setColorRGB((byte)RIGHT_BLINKER, 0, 0, 0);
   while (millis() - now < 2000)
   {
     leds.setColorRGB((byte)LEFT_BLINKER, 255, 100, 0);
+    digitalWrite(LEFT_BLINKER_LED, HIGH);
     delay(600);
     leds.setColorRGB((byte)LEFT_BLINKER, 0, 0, 0);
+    digitalWrite(LEFT_BLINKER_LED, LOW);
     delay(600);
   }
 
