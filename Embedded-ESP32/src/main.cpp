c290ca463c5b0ef8a6857d10f3c3b584acdf6eb6
I-Badier <isaure.badier@gmail.com>
2022-11-24 10:13:28 +0100

    Blinkers + Light sensor
    Clignotants fixes pour le moment (s'allument pour 2 secondes).
    TO DO : ajouter interruptions sur des boutons pour activer les clignotants

--- Embedded-ESP32/src/main.cpp
+++ Embedded-ESP32/src/main.cpp
@@ -1,14 +1,47 @@
 #include <Arduino.h>
+#include <ChainableLED.h>
 
-void setup() {
+#define NUM_LED 3
+#define LIGHT_SENSOR_PIN 0
+
+#define LEFT_BLINKER 0
+#define RIGHT_BLINKER 1
+
+ChainableLED leds = ChainableLED(7, 8, NUM_LED);
+
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("-- SETUP --");
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LIGHT_SENSOR_PIN, INPUT);
+  Serial.println("- END OF SETUP --");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(2000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(2000);
+void loop()
+{
+  int light_value = analogRead(LIGHT_SENSOR_PIN);
+  Serial.print("Luminosity : ");
+  Serial.println(light_value);
+
+  long int now = millis();
+  leds.setColorRGB((byte)RIGHT_BLINKER, 0, 0, 0);
+  while (millis() - now < 2000)
+  {
+    leds.setColorRGB((byte)LEFT_BLINKER, 255, 100, 0);
+    delay(600);
+    leds.setColorRGB((byte)LEFT_BLINKER, 0, 0, 0);
+    delay(600);
+  }
+
+  now = millis();
+  leds.setColorRGB((byte)LEFT_BLINKER, 0, 0, 0);
+  while (millis() - now < 2000)
+  {
+    leds.setColorRGB((byte)RIGHT_BLINKER, 255, 100, 0);
+    delay(600);
+    leds.setColorRGB((byte)RIGHT_BLINKER, 0, 0, 0);
+    delay(600);
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 4I4U-Team-1
 Tricycle avec deux roues motrices. Allumage automatique des feux et système de clignotants.
+
+## Embedded ESP32
+Code embarqué sur le vélo
+24/11 :  
+- 2 LED chaînées pour les clignotants. LED numéro 1 pour cligno gauche, numéro 2 pour cligno droit (constantes pré-processeurs). Clignotement normé à 90 par minutes.  
+Fonctionne en 5V, pin 7 fil jaune (clock) et pin 8 fil blanc (data)
+- Capteur de luminosité. Fonctionne en 5V, pin 0 pour un canal d'ADC. Valeurs de 0 (noir complet) à 4095 (luminosité ambiante). Sensibilité à la luminosité assez faible, sûrement à changer pour le prototype. 
+
