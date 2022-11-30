10c32abe572ecdc1c803a5e06597e87f7358ffa4
I-Badier <isaure.badier@gmail.com>
2022-11-30 16:20:53 +0100

    ISR button and leds

--- Embedded-ESP32/src/main.cpp
+++ Embedded-ESP32/src/main.cpp
@@ -18,46 +18,128 @@ volatile bool start_left_blinkers = false;
 volatile bool start_right_blinkers = false;
 volatile long int last_push_left = 0;
 volatile long int last_push_right = 0;
+hw_timer_t *timer_left_blinkers = NULL;
+hw_timer_t *timer_right_blinkers = NULL;
+volatile bool state = false;
+
+volatile bool left_blinkers_on = false;
+volatile bool right_blinkers_on = false;
 
 void IRAM_ATTR ISR_left_blinkers_button()
 {
-  if (!start_right_blinkers)
+  ets_printf("Left");
+  if (!start_left_blinkers)
   {
-    if ((millis() - last_push_left) > 100)
-    {
-      ets_printf("Left pressed\n");
-      last_push_left = millis();
-      if (start_left_blinkers)
-      {
-        start_left_blinkers = false;
-      }
-      else
-      {
-        start_left_blinkers = true;
-      }
-    }
+    start_left_blinkers = true;
+    left_blinkers_on = true;
+    digitalWrite(LEFT_BLINKER_LED, HIGH);
+    leds.setColorRGB((byte)LEFT_BLINKER, 255, 100, 0);
+    timerAlarmEnable(timer_left_blinkers);
+    timerRestart(timer_left_blinkers);
+  }
+  else
+  {
+    start_left_blinkers = false;
+    left_blinkers_on = false;
+    digitalWrite(LEFT_BLINKER_LED, LOW);
+    leds.setColorRGB((byte)LEFT_BLINKER, 0, 0, 0);
+    timerAlarmDisable(timer_left_blinkers);
   }
 }
 
 void IRAM_ATTR ISR_right_blinkers_button()
 {
-  if (!start_left_blinkers)
+  ets_printf("Right");
+  if (!start_right_blinkers)
   {
-    if ((millis() - last_push_right) > 100)
-    {
-      ets_printf("Right pressed\n");
-      last_push_right = millis();
-      if (start_right_blinkers)
-      {
-        start_right_blinkers = false;
-      }
-      else
-      {
-        start_right_blinkers = true;
-      }
-    }
+    start_right_blinkers = true;
+    right_blinkers_on = true;
+    digitalWrite(RIGHT_BLINKER_LED, HIGH);
+    leds.setColorRGB((byte)RIGHT_BLINKER, 255, 100, 0);
+    timerAlarmEnable(timer_right_blinkers);
+    timerRestart(timer_right_blinkers);
+  }
+  else
+  {
+    start_right_blinkers = false;
+    right_blinkers_on = false;
+    digitalWrite(RIGHT_BLINKER_LED, LOW);
+    leds.setColorRGB((byte)RIGHT_BLINKER, 0, 0, 0);
+    timerAlarmDisable(timer_right_blinkers);
   }
 }
+
+void IRAM_ATTR ISR_left_timer()
+{
+  if (left_blinkers_on)
+  {
+    left_blinkers_on = false;
+    digitalWrite(LEFT_BLINKER_LED, HIGH);
+    leds.setColorRGB((byte)LEFT_BLINKER, 255, 100, 0);
+  }
+  else
+  {
+    left_blinkers_on = true;
+    digitalWrite(LEFT_BLINKER_LED, LOW);
+    leds.setColorRGB((byte)LEFT_BLINKER, 0, 0, 0);
+  }
+}
+
+void IRAM_ATTR ISR_right_timer()
+{
+  if (right_blinkers_on)
+  {
+    right_blinkers_on = false;
+    digitalWrite(RIGHT_BLINKER_LED, HIGH);
+    leds.setColorRGB((byte)RIGHT_BLINKER, 255, 100, 0);
+  }
+  else
+  {
+    right_blinkers_on = true;
+    digitalWrite(RIGHT_BLINKER_LED, LOW);
+    leds.setColorRGB((byte)RIGHT_BLINKER, 0, 0, 0);
+  }
+}
+
+// void IRAM_ATTR ISR_left_blinkers_button()
+// {
+//   if (!start_right_blinkers)
+//   {
+//     if ((millis() - last_push_left) > 100)
+//     {
+//       ets_printf("Left pressed\n");
+//       last_push_left = millis();
+//       if (start_left_blinkers)
+//       {
+//         start_left_blinkers = false;
+//       }
+//       else
+//       {
+//         start_left_blinkers = true;
+//       }
+//     }
+//   }
+// }
+
+// void IRAM_ATTR ISR_right_blinkers_button()
+// {
+//   if (!start_left_blinkers)
+//   {
+//     if ((millis() - last_push_right) > 100)
+//     {
+//       ets_printf("Right pressed\n");
+//       last_push_right = millis();
+//       if (start_right_blinkers)
+//       {
+//         start_right_blinkers = false;
+//       }
+//       else
+//       {
+//         start_right_blinkers = true;
+//       }
+//     }
+//   }
+// }
 
 void setup()
 {
@@ -74,6 +156,14 @@ void setup()
   attachInterrupt(LEFT_BLINKER_BUTTON, ISR_left_blinkers_button, FALLING);
   attachInterrupt(RIGHT_BLINKER_BUTTON, ISR_right_blinkers_button, FALLING);
 
+  timer_left_blinkers = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer_left_blinkers, &ISR_left_timer, true);
+  timerAlarmWrite(timer_left_blinkers, 600000, true);
+
+  timer_right_blinkers = timerBegin(1, 80, true);
+  timerAttachInterrupt(timer_right_blinkers, &ISR_right_timer, true);
+  timerAlarmWrite(timer_right_blinkers, 600000, true);
+
   leds.setColorRGB((byte)RIGHT_BLINKER, 0, 0, 0);
   leds.setColorRGB((byte)LEFT_BLINKER, 0, 0, 0);
 
@@ -82,39 +172,4 @@ void setup()
 
 void loop()
 {
-  if (start_left_blinkers)
-  {
-    while (start_left_blinkers)
-    {
-      leds.setColorRGB((byte)LEFT_BLINKER, 255, 100, 0);
-      digitalWrite(LEFT_BLINKER_LED, HIGH);
-      delay(600);
-      leds.setColorRGB((byte)LEFT_BLINKER, 0, 0, 0);
-      digitalWrite(LEFT_BLINKER_LED, LOW);
-      delay(600);
-    }
-  }
-  else
-  {
-    leds.setColorRGB((byte)LEFT_BLINKER, 0, 0, 0);
-    digitalWrite(LEFT_BLINKER_LED, LOW);
-  }
-
-  if (start_right_blinkers)
-  {
-    while (start_right_blinkers)
-    {
-      leds.setColorRGB((byte)RIGHT_BLINKER, 255, 100, 0);
-      digitalWrite(RIGHT_BLINKER_LED, HIGH);
-      delay(600);
-      leds.setColorRGB((byte)RIGHT_BLINKER, 0, 0, 0);
-      digitalWrite(RIGHT_BLINKER_LED, LOW);
-      delay(600);
-    }
-  }
-  else
-  {
-    leds.setColorRGB((byte)RIGHT_BLINKER, 0, 0, 0);
-    digitalWrite(RIGHT_BLINKER_LED, LOW);
-  }
 }
